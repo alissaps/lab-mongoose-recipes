d2d15aa34260bf566eaa27f9c011b68f0d91ae37
Alissa Seixas <alissaps@gmail.com>
2021-12-01 22:04:59 -0300

    done

--- index.js
+++ index.js
@@ -15,8 +15,54 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(async () => {
     // Run your code here, after you have insured that the connection was made
+    try {
+      const createRecipe = await Recipe.create({
+        title: "Vegan Brownies",
+        level: "Amateur Chef",
+        ingredients: [
+          "2 cups unbleached all-purpose flour",
+          "2 cups white sugar",
+          "¾ cup unsweetened cocoa powder",
+          "1 teaspoon baking powder",
+          "1 teaspoon salt",
+          "1 cup water",
+          "1 cup vegetable oil",
+          "1 teaspoon vanilla extract",
+        ],
+        cuisine: "French",
+        dishType: "dessert",
+        image:
+          "https://img.cybercook.com.br/receitas/690/brownie-low-carb-com-nozes.jpeg",
+        duration: 30,
+        creator: "Alissa",
+      });
+
+      console.log(`Title: ${createRecipe.title}`)
+
+      const addRecipes = await Recipe.insertMany(data);
+      addRecipes.map((element) => {
+        console.log(`Title: ${element.title}`)
+      });
+
+      const updateRecipes = await Recipe.findOneAndUpdate(
+        {title: "Rigatoni alla Genovese"},
+        {duration: 100},
+        {new: true}
+      );
+
+      console.log(`Receita Atualizada: ${updateRecipes}`)
+
+      const removeRecipe = await Recipe.deleteOne(
+        {title: "Carrot Cake"}
+      )
+      console.log(removeRecipe)
+
+    }catch (err) {
+      console.log(err);
+    }
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
